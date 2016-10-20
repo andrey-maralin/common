4f3d74c343daa1d5a05591079ed370f0f336a564
Andrey Maralin <a.a.maralin@gmail.com>
2016-10-20 11:20:49 +0300

    cat func

---
+++ cat/cat.py
@@ -0,0 +1,22 @@
+from sys import stdout
+result_file_name = 'result.txt'
+
+
+def cat(*args):
+    line_number = 0
+    with stdout as out:
+        for file_name in args:
+            try:
+                with open(file_name, 'r') as f:
+                    for line in f:
+                        line_number += 1
+                        out.write(line + '\n')
+            except IOError:
+                print "File {0} wasn't found!".format(file_name)
+                continue
+        print
+    return line_number
+
+
+if __name__ == "__main__":
+    cat('1.txt', '2.css')
