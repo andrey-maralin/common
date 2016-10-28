e89080bbcf9990d1ed1b5dc400aa68623995927f
Andrey Maralin <a.a.maralin@gmail.com>
2016-10-28 13:25:03 +0300

    text parser

--- text_parser/process_text.py
+++ text_parser/process_text.py
@@ -32,7 +32,12 @@ def get_result(text):
 
 
 if __name__ == "__main__":
+    args = sys.argv[1]
+    if len(args) < 2:
+        print 'Вы забыли ввести текс.'
+
     text = sys.argv[1].decode("cp1251")
+
     if is_valid_text(text):
         get_result(text)
     else:
